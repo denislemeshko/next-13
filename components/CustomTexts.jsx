6f2d9dddd5894ac7d7d00b292f6ac2d07418e9fc
denislemeshko <lemeshko.denis@yandex.ru>
2022-12-18 16:52:09 +0300

    folder out

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -7,13 +7,12 @@ import { textContainer, textVariant2 } from '../utils/motion';
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
     variants={textContainer}
-    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
-  >
-    {Array.from(title).map((letter, index) => (
-      <motion.span variants={textVariant2} key={index}>
-        {letter === '' ? '\u00A0' : letter}
-      </motion.span>
-    ))}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}>
+      {Array.from(title).map((letter, index) => (
+        <motion.span variants={textVariant2} key={index}>
+          {letter==='' ? '\u00A0': letter}
+        </motion.span>
+      ))}
   </motion.p>
 );
 
